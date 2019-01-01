80be3e310dea59caec317f3038d0ae85167ac0ba
DanMGZ <spacehodling@gmail.com>
2019-01-01 12:05:16 -0500

    added starter code

---
+++ TextFileChallenge/ChallengeForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileChallenge
+{
+    public partial class ChallengeForm : Form
+    {
+        BindingList<UserModel> users = new BindingList<UserModel>();
+
+        public ChallengeForm()
+        {
+            InitializeComponent();
+
+            WireUpDropDown();
+        }
+
+        private void WireUpDropDown()
+        {
+            usersListBox.DataSource = users;
+            usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            users.Add(new UserModel { FirstName = firstNameText.Text, LastName = lastNameText.Text, Age = (int)agePicker.Value, IsAlive = isAliveCheckbox.Checked });
+        }
+    }
+}
